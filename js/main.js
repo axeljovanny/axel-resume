b1eb7238534e7a6648e11029945e01cd4e10e0a4
Axel Jovanny Quintero Trujillo <axel.jovanny.aj@gmail.com>
2019-11-25 00:19:12 -0600

    Info
    Se agrega mi info y skills

--- js/main.js
+++ js/main.js
@@ -171,10 +171,10 @@
             scale: true, // set 'false' to stop scaling
             blend: true, // set 'false' to stop glitch blending
             blendModeType: 'hue', // select blend mode type
-            glitch1TimeMin: 600, // set min time for glitch 1 elem
+            glitch1TimeMin: 1, // set min time for glitch 1 elem
             glitch1TimeMax: 900, // set max time for glitch 1 elem
-            glitch2TimeMin: 10, // set min time for glitch 2 elem
-            glitch2TimeMax: 115, // set max time for glitch 2 elem
+            glitch2TimeMin: 1, // set min time for glitch 2 elem
+            glitch2TimeMax: 900, // set max time for glitch 2 elem
             zIndexStart: 8, // because of absolute position, set z-index base value
         });
     };
