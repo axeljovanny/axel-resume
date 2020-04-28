27ef7d40d76ec848ce6961965f69ad4b1a0ba4c0
Axel Jovanny Quintero Trujillo <axel.jovanny.aj@gmail.com>
2020-04-27 22:32:21 -0500

    Ajustes a CV despues de Tecniva

--- js/main.js
+++ js/main.js
@@ -166,15 +166,15 @@
     // Glitch effect
     var glitch = function() {
         $(".glitch-img").mgGlitch({
-            destroy : false, // set 'true' to stop the plugin
+             destroy : true, // set 'true' to stop the plugin
             glitch: true, // set 'false' to stop glitching
-            scale: false, // set 'false' to stop scaling
-            blend : true, // set 'false' to stop glitch blending
-            blendModeType : 'screen', // select blend mode type
-            glitch1TimeMin : 10, // set min time for glitch 1 elem
-            glitch1TimeMax : 100, // set max time for glitch 1 elem
+            scale: true, // set 'false' to stop scaling
+            blend : false, // set 'false' to stop glitch blending
+            blendModeType : 'hue', // select blend mode type
+            glitch1TimeMin : 600, // set min time for glitch 1 elem
+            glitch1TimeMax : 300, // set max time for glitch 1 elem
             glitch2TimeMin : 10, // set min time for glitch 2 elem
-            glitch2TimeMax : 100, // set max time for glitch 2 elem
+            glitch2TimeMax : 115, // set max time for glitch 2 elem
             zIndexStart : 8, // because of absolute position, set z-index base value
         });
     };
